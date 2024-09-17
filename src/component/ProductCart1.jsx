c307c813edd711db07163384f89563c726f04c71
Ruhul-Amin-Badol <ruhul.gub@gmail.com>
2024-09-17 22:33:10 +0600

    card update

--- src/component/ProductCart1.jsx
+++ src/component/ProductCart1.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Productcart1 from "../assets/images/ProductCart1.jpg";
 import { PiRectangleFill } from "react-icons/pi";
-import { CiSearch, CiLocationOn } from "react-icons/ci";
+import { CiLocationOn } from "react-icons/ci";
+import { BiSolidImage } from "react-icons/bi";
 
 const ProductCart1 = () => {
   return (
     <div className="ProductCart1-main shadow-lg">
       <div className="ProductCart-image mb-0">
-        <img src={Productcart1} alt="hero" className="img-fluid mb-0" />
+        <img src={Productcart1} alt="hero" className="img-fluid mb-0 ProductCart-img"/>
+        <span className="image-count"><BiSolidImage className="icon"/> 20</span>
       </div>
       <div className="ProductCart1-main2 mt-0">
         <div className="d-flex justify-content-between">
@@ -17,7 +19,7 @@ const ProductCart1 = () => {
         <hr className="mx-4 mt-0"/>
         <div>
             <p className="productcart-p1 mb-0">SunnySlope Suites</p>
-            <p className="productcart-p2 mt-0"><CiLocationOn style={{ color: '#f06711'}} className="text-bold"/> Meadowshire Park, Greenfield, USA</p>
+            <p className="productcart-p2 mt-0"><CiLocationOn className="icon"/> Meadowshire Park, Greenfield, USA</p>
         </div>
         <div>
             <p className='productcart-Budget'>$ 250000</p>
